4843a0c397a2fc4930c08ac7f403d29e15ff034d
antfromano <antfromano@gmail.com>
2022-06-26 16:34:46 +0000

    edited and updated map LatLng in google-maps.js

--- static/website/js/google-map.js
+++ static/website/js/google-map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(42.372161115429485, -71.56119092130807);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 11,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
