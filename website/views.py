5187a67a05d04eee16cd2d438214cabdd37850cc
antfromano <antfromano@gmail.com>
2022-06-26 17:02:03 +0000

    editing and updating contact form, views.py and urls.py

--- website/views.py
+++ website/views.py
@@ -4,4 +4,12 @@ def home(request):
     return render(request, 'home.html', {})
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == "POST":
+        name == request.POST['name']
+        email == request.POST['email']
+        subject == request.POST['subject']
+        message == request.POST['message']
+
+        return render(request, 'contact.html', {'name': name})
+    else:
+        return render(request, 'contact.html', {})
